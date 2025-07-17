00298d0ef14a2e4da48d16d86b2afe2923b62a66
Subav11 <subhamyadav7528@gmail.com>
2025-07-17 15:06:35 +0530

    update

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,14 +3,18 @@ import axios from "axios";
 import { AppContext } from "../App";
 export default function Product() {
   const API_URL = import.meta.env.VITE_API_URL;
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(5);
+  const [totalPages, setTotalPages] = useState(1);
   const [products, setProducts] = useState([]);
   const [error, setError] = useState();
   const { user, cart, setCart } = useContext(AppContext);
   const fetchProducts = async () => {
     try {
-      const url = `${API_URL}/api/products/all`;
+      const url = `${API_URL}/api/products/all?page=${page}&limit=${limit}`;
       const result = await axios.get(url);
       setProducts(result.data.products);
+      setTotalPages(result.data.total);
     } catch (err) {
       console.log(err);
       setError("Something went wrong");
@@ -18,7 +22,7 @@ export default function Product() {
   };
   useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [page]);
 
   const addToCart = (product) => {
     const found = cart.find((item) => item._id === product._id);
@@ -32,13 +36,25 @@ export default function Product() {
       {products &&
         products.map((product) => (
           <div key={product._id}>
-            <img src={product.imgUrl} width={100}/>
+            <img src={product.imgUrl} width={100} />
             <h3>{product.productName}</h3>
             <p>{product.description}</p>
             <h4>{product.price}</h4>
             <button onClick={() => addToCart(product)}>Add to Cart</button>
           </div>
         ))}
+      <div>
+        <button disabled={page === 1} onClick={() => setPage(page - 1)}>
+          Previous
+        </button>
+        {page} of {totalPages}
+        <button
+          disabled={page === totalPages}
+          onClick={() => setPage(page + 1)}
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 }
